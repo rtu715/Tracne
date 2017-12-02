8a178ee1c055381ec7ec3e555ac00bb4e8f4b4db
Helena Wu <helenawu@Helenas-MacBook-Air.local>
2017-12-02 17:11:15 -0500

    Add google image to landing page

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ProfilePage } from './JavaScript/Views/ProfilePage';
-import { RootNavigator } from './JavaScript/Views/RootNavigator';
+import { StyleSheet, Text, View, Image } from 'react-native';
+//import { ProfilePage } from './JavaScript/Views/ProfilePage';
+// import { EventArgs } from './JavaScript/Views/EventArgs';
 
 /**
  * the landing page.
  */
+//  var EventArgs = require('./JavaScript/Views/EventArgs.js')
 export default class App extends React.Component {
   render() {
+     let pic = {
+     uri: 'https://www.makeupforever.com/sites/default/files/styles/field_how_to_step__field_howto_step_image__full__how_to_page/public/1.6_Contouring-Square-Face.jpg?itok=ObBIMZPp'
+ };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Welcome to TrAcne!</Text>
+        <Text>How is your skin today? </Text>
+        <Image source={pic} style={{width: 300, height: 300}}/>
+        <Text>How are you?</Text>
       </View>
     );
   }
