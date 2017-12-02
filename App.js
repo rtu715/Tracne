dde44073e0805ba35ca18c933ea26a64e23fa1e0
Alan Armero <aa3938@columbia.edu>
2017-12-02 13:34:13 -0500

    consolidated directories

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+/**
+ * the landing page
+ */
 export default class App extends React.Component {
   render() {
     return (
